962d79bef22a00b33f39a0cc06e5d0a95a503abc
Moamen-Wady <moamen.wady@gmail.com>
2026-05-04 15:51:11 +0300

    changes: mongo=>postgres / atlas=>neon / mongoose=>drizzle / index.ts=>app.ts / bun=>node + latest version of all packages

--- app.ts
+++ app.ts
@@ -12,6 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "http://localhost:3005",
       "https://ramadan-bags.vercel.app",
       "https://ramadan-bags.pages.dev",
     ],
